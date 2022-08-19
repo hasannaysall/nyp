d58b29207590f783bac47079b6f6566ca658020f
Hasan Aysal <103414704+hasannaysall@users.noreply.github.com>
2022-08-19 23:46:43 +0300

    Update test.java

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -2,10 +2,11 @@
 public class test {
     public static void main(String[] args) {
         
-    araba araba1 = new araba();
     araba1.setModel("bmw");
     araba1.setRenk("siyah");
+    araba1.setMotor(16);
         System.out.println(araba1.getModel());
-         System.out.println(araba1.getRenk());   
+         System.out.println(araba1.getRenk()); 
+         System.out.println(araba1.getMotor());
 }
 }
