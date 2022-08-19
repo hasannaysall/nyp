2957f5d4dd994b9f156b73689ec77e5c147c8124
Hasan Aysal <103414704+hasannaysall@users.noreply.github.com>
2022-08-19 22:36:54 +0300

    Initial commit

---
+++ src/main/java/araba.java
@@ -0,0 +1,49 @@
+
+public class araba {
+    private String model;
+    private String renk;
+    private int motor;
+
+    /**
+     * @return the model
+     */
+    public String getModel() {
+        return model;
+    }
+
+    /**
+     * @param model the model to set
+     */
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    /**
+     * @return the renk
+     */
+    public String getRenk() {
+        return renk;
+    }
+
+    /**
+     * @param renk the renk to set
+     */
+    public void setRenk(String renk) {
+        this.renk = renk;
+    }
+
+    /**
+     * @return the motor
+     */
+    public int getMotor() {
+        return motor;
+    }
+
+    /**
+     * @param motor the motor to set
+     */
+    public void setMotor(int motor) {
+        this.motor = motor;
+    }
+    
+}
---
+++ src/main/java/test.java
@@ -0,0 +1,10 @@
+
+public class test {
+    public static void main(String[] args) {
+        
+    araba araba1 = new araba();
+    araba1.setModel("bmw");
+        System.out.println(araba1.getModel());
+            
+}
+}
