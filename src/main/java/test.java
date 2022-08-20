657c3ecef579acbd075cb7ffec965edcaf100f7f
Hasan Aysal <103414704+hasannaysall@users.noreply.github.com>
2022-08-20 14:17:24 +0300

    renk degisti

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -4,7 +4,7 @@ public class test {
         
     araba araba1 = new araba();
     araba1.setModel("bmw");
-    araba1.setRenk("beyaz");
+    araba1.setRenk("mavi");
     araba1.setMotor(16);
         System.out.println(araba1.getModel());
          System.out.println(araba1.getRenk()); 
